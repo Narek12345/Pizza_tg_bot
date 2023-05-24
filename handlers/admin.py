6918827b78ed7445de6beccdaf50eca04677cd0d
Narek <narekbayanduryan16@gmail.com>
2023-05-24 20:52:59 +0300

    Wrote an inline button to delete from the database for the admin.

--- handlers/admin.py
+++ handlers/admin.py
@@ -2,6 +2,7 @@ from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from create_bot import dp, bot
 from data_base import sqlite_db
@@ -10,12 +11,13 @@ from keyboards import admin_kb
 ID = None
 
 
-# Получаем ID текущего модератора.
-@dp.message_handler(commands=['moderator'], is_chat_admin=True)
 async def make_changes_command(message: types.Message):
+    """Получаем ID текущего пользователя."""
     global ID
     ID = message.from_user.id
-    await bot.send_message(message.from_user.id, "Что хозяин надо ?", reply_markup=admin_kb.button_case_admin)
+    await bot.send_message(message.from_user.id,
+                           "Что хозяин надо ?",
+                           reply_markup=admin_kb.button_case_admin)
     await message.delete()
 
 
@@ -65,19 +67,34 @@ async def load_price(message: types.Message, state: FSMContext):
     if message.from_user.id == ID:
         async with state.proxy() as data:
             data['price'] = float(message.text)
-    
-        await sqlite_db.sql_add_command(state)    
+
+        await sqlite_db.sql_add_command(state)
         await state.finish()
 
 
-# Выход из состояния машинных состояний.
 async def cancel_handler(message: types.Message, state: FSMContext):
+    """Выход из состояния машинных состояний."""
     if message.from_user.id == ID:
         current_state = await state.get_state()
         if current_state is None:
             return
         await state.finish()
         await message.reply('Ок')
+
+
+@dp.callback_query_handler(lambda x: x.data and x.data.startswith('del '))
+async def del_callback_run(callback_query: types.CallbackQuery):
+    await sqlite_db.sql_delete_command(callback_query.data.replace('del ', ''))
+    await callback_query.answer(text=f'{callback_query.data.replace("del ", "del")} удалена.', show_alert=True)
+
+
+@dp.message_handler(commands=['Удалить'])
+async def delete_item(message: types.Message):
+    if message.from_user.id == ID:
+        read = await sqlite_db.sql_read2()
+        for ret in read:
+            await bot.send_photo(message.from_user.id, ret[0], f'{ret[1]}\nОписание: {ret[2]}\nЦена: {ret[-1]}')
+            await bot.send_message(message.from_user.id, text='^^^', reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton(f'Удалить {ret[1]}', callback_data=f'del {ret[1]}')))
 
 
 def register_handlers_admin(dp: Dispatcher):
